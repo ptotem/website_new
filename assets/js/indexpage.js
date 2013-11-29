5fef62ad5f3049d7738b72e45bbf5d012d48c7cf
Rakesh Rengaraj <rakesh.rengaraj@gmail.com>
2013-11-29 19:52:38 +0530

    updated acc to mail

--- assets/js/indexpage.js
+++ assets/js/indexpage.js
@@ -1,35 +1,41 @@
 //For About Us Windy Array Start
 
-var img_array = ["assets/images/arijit.jpg","assets/images/kamalika.jpg","assets/images/arun.jpg","assets/images/dipanwita.jpg","assets/images/rushabh.jpg","assets/images/sunny.jpg","assets/images/ashwin.jpg","assets/images/amol.jpg","assets/images/rakesh.jpg","assets/images/nilesh.jpg","assets/images/perseus.jpg","assets/images/deepak.jpg","assets/images/swapnil.jpg","assets/images/nikunj.jpg","assets/images/yashodeep.jpg"]
-var name_array=["Arijit Lahiri","Kamalika Bhattacharya","Arunkumar Balasubramanian","Dipanwita Kar","Rushabh Hathi","Sunny Singh","Ashwin Kumar","Amol Bhor","Rakesh Rengaraj","Nilesh Panchal","Perseus Vazifdar","Deepak Gawas","Swapnil Domade","Nikunj Thakkar","Yashodeep Talele"]
-var text_array = ['Arijit Lahiri aka "Lolly" says he is a dreamer, thinker and a philosopher. We say he dreams, thinks and philosophises. His wife - Kamal, ofcourse, thinks he is an idiot.',
-                  'Kamalika Bhattacharya aka "Kamal" is the driving force of the firm and Arijit ofcourse.She is the sort who lives for others - you can tell the others by their hunted expressions',
-                  'Arunkumar Balasubramanian aka AKB had been a comfortable corporate banker when asudden was bug bitten and became an unknown entity in our firm. People dont deem him human anymore given that he eats - hardly, sleeps - hardly or even works - hardly!',
-                  'Dipanwita Kar aka "Deepanwita" has a rather demure persona. But whenever she is not out business development, she does the code testing. This is when she makes the programmers  run faster than Usain Bolt',
-                  'Rushabh Hathi aka "Hathi” would rather deal with alien Artificial Intelligence than simple common mortals. His passion can only be defined in binary - literally and is the tech expert of our firm',
-                  'Sunny Singh aka “Sunny Sardar” has a fire breathing dragon’s temper and loves hacking and smacking. Oh! He is an engineer too.',
-                  'Ashwin Kumar aka “Ash” has tried his hand at quite a few things before boarding our train as “Product Manager”. And here he specializes in multitasking and err… “tasking”–literally.',
-                  'Amol Bhor aka "amol" acts rather than speak - literally. He does everything here admin, HR, finance, accounts, programming… the list goes on. His rumoured linkups with various women would put bollywood actors to shame.',
-                  'Rakesh Rengaraj aka "Ra Kya?" decided to become a software programmer and he does that with elan and panache. His quick wit and remarks can shrink time. But it did backfire when a girl called him a "Tapori".',
-                  'Nilesh Panchal aka "Neelu" is a true blue engineer by heart and profession - his of course. His best kept secret is adorning the super hero - "Code Man" avatar.',
-                  'Perseus Vazifdar aka “PL/ HB/ RL etc… walked in as an animator, designer and game creator. He was welcomed right away with numerous stares when he stated, “Let’s play it this way.” Ofcourse he never plays anything just one way.',
-                  'Deepak Gawas aka “Gawsya” was nuts to pursue four years in advertising before sense prevailed and he took up a banking job - for just a day. Becoming a nut again, he joined us. Get him angry and you get a million bucks – figuratively that is.',
-                  'Swapnil Damode aka "Sapya" paints pictures as he speaks. An "artist" because he paints. A "designer" because he designs. The trouble with him is that even as you explain to him the design requirement, he has already sent them to you.',
-                  'Nikunj Thakkar aka "NikiTha" is a serious software programmer with a keen ear for music.His out of box thinking is defined as he says "If you are there before its over, you are on time"',
-                  'Yashodeep Talele aka "Yash" is an expert loafer when he is not writing books on databases.His current excitement in life is in publishing his handbook on Oracle that one can actually carry in their hands.']
+var img_array = ["assets/images/arijit.jpg","assets/images/kamalika.jpg","assets/images/arun.jpg","assets/images/dipanwita.jpg","assets/images/rushabh.jpg","assets/images/sunny.jpg","assets/images/ashwin.jpg","assets/images/amol.jpg","assets/images/rakesh.jpg","assets/images/nilesh.jpg","assets/images/perseus.jpg","assets/images/deepak.jpg","assets/images/swapnil.jpg","assets/images/nikunj.jpg"]
+var name_array=["Arijit Lahiri","Kamalika Bhattacharya","Arunkumar Balasubramanian","Dipanwita Kar","Rushabh Hathi","Sunny Singh","Ashwin Kumar","Amol Bhor","Rakesh Rengaraj","Nilesh Panchal","Perseus Vazifdar","Deepak Gawas","Swapnil Domade","Nikunj Thakkar"]
+var text_array = ["Arijit is an alumnus of the prestigious IIM, Calcutta, Arijit has over ten years of experience spanning a myriad of functions within Corporate Banking & Wealth Management. He has held national level positions at large Private & Investment Banks. Together with Kamalika, Ptotem is his initiative.",
+                  "Kamalika is an MBA from the Indian School of Business, Hyderabad. She currently holds a senior position with a large-cap Investment Bank. Ptotem Learning Projects is essentially her brain-child. She remains a strategic partner of the firm.",
+                  "Arun is an MBA from NMIMS. Arun has a work experience of almost a decade & half spanning senior positions in International Trade Finance & Consumer Banking. Arun is a partner at the firm h&ling its Finance & Operations.",
+                  "Dipanwita is an MBA from the Indian School of Business, Hyderabad. Dipanwita heads the Sales & Business Development for Ptotem. Her career has involved stints with Infosys, Nokia, Alchemy Capital Management & Religare Macquarie Private Wealth in development & sales.",
+                  "Rushabh holds a Post Graduate degree in Artificial Intelligence from the University of Edinburgh. He is Ptotem’s technology wizard & is in charge of technology initiatives for new product development.",
+                  "Sunny is an engineering graduate in Computer Science from the Mumbai University. Prior to joining us he worked with Patni Computer Systems. Sunny heads the Development Team at Ptotem.",
+                  "Ashwin majored in Botany & is also a Certified Financial Analyst. Apart from being instrumental in initiating several startups, Ashwin has experience in the field of Equity Analysis. At Ptotem, Ashwin heads Product Development & Marketing.",
+                  "Amol is an engineering graduate in Computer Science. Amol spearheads the workflow management of the firm. He also manages the Human Resources,Coding, General Administration & Accounts.",
+                  "Rakesh is an engineering graduate in Electronics & Telecommunication. After a brief stint with Reliance Infocom, he has joined the team of Ptotem's software developers.",
+                  "Nilesh is an engineering graduate who started his career with Ptotem Learning Projects. Nilesh is a part of the firm's software developers.",
+                  "Perseus is a Post Graduate in commerce & holds a diploma in multimedia from ‘Arena Multimedia’. He joined the ranks of Ptotem as a Designer & is now a Project Manager.",
+                  "Deepak is a graduate in mass media with a specialization in advertising. He tried his hands in the corporate world with a very short stint with Bank of Maharashtra before moving to Ptotem as a Business Development Manager.",
+                  "Swapnil graduated in the field of Fine Arts from Central India School of Fine Arts. Before joining Ptotem as the Lead designer, he played an instrumental role at a startup that specializes in Advertising & Design.",
+                  "Nikunj is an engineering graduate in Computer Science. Prior to joining Ptotem as a Senior Software Developer, Nikunj has worked with Accenture. Nikunj is also a founding partner of ‘Yuva Beat’ – a student driven startup."]
 
 //For About Us Windy Array End
 
 //For Product Bookblock Array Start
-var product_img_array = ["assets/images/products/Slide1.JPG", "assets/images/products/Slide2.JPG", "assets/images/products/Slide3.JPG", "assets/images/products/Slide4.JPG", "assets/images/products/Slide5.JPG", "assets/images/products/Slide6.JPG", "assets/images/products/Slide7.JPG", "assets/images/products/Slide8.JPG", "assets/images/products/Slide9.JPG", "assets/images/products/Slide10.JPG", "assets/images/products/Slide11.JPG", "assets/images/products/Slide12.JPG", "assets/images/products/Slide13.JPG", "assets/images/products/Slide14.JPG", "assets/images/products/Slide15.JPG", "assets/images/products/Slide16.JPG", "assets/images/products/Slide17.JPG", "assets/images/products/Slide18.JPG"]
+var product_img_array = ["assets/images/products//Slide01.jpg", "assets/images/products/Slide02.jpg", "assets/images/products/Slide03.jpg", "assets/images/products/Slide04.jpg","assets/images/products/Slide05.jpg","assets/images/products/Slide06.jpg","assets/images/products/Slide07.jpg","assets/images/products/Slide08.jpg","assets/images/products/Slide09.jpg","assets/images/products/Slide10.jpg","assets/images/products/Slide11.jpg","assets/images/products/Slide12.jpg","assets/images/products/Slide13.jpg","assets/images/products/Slide14.jpg","assets/images/products/Slide15.jpg","assets/images/products/Slide16.jpg","assets/images/products/Slide17.jpg","assets/images/products/Slide18.jpg","assets/images/products/Slide19.jpg","assets/images/products/Slide20.jpg","assets/images/products/Slide21.jpg","assets/images/products/Slide22.jpg","assets/images/products/Slide23.jpg"]
 //For Product Bookblock Array End
 
+//For easy game intro Bookblock Array Start
+var easy_game_img_array = ["assets/images/easy/serious_games_ppt_easy_Page_1.jpg", "assets/images/easy/serious_games_ppt_easy_Page_2.jpg", "assets/images/easy/serious_games_ppt_easy_Page_3.jpg", "assets/images/easy/serious_games_ppt_easy_Page_4.jpg", "assets/images/easy/serious_games_ppt_easy_Page_5.jpg"]
+//For easy game intro Bookblock Array End
 
-
+//For easy game intro Bookblock Array Start
+var hard_game_img_array = ["assets/images/hard/serious_games_ppt_hard_Page_1.jpg", "assets/images/hard/serious_games_ppt_hard_Page_2.jpg", "assets/images/hard/serious_games_ppt_hard_Page_3.jpg", "assets/images/hard/serious_games_ppt_hard_Page_4.jpg", "assets/images/hard/serious_games_ppt_hard_Page_5.jpg"]
+//For easy game intro Bookblock Array End
 
 
 
 $(function () {
+    $('body').fadeIn(5000);
+
     $(".close_parallax_btn").hide();
 
 
@@ -76,7 +82,7 @@ $(function () {
         flipside('products');
         $('#gamify').css("pointer-events", "none");
 
-        products_bookblock(product_img_array);
+        products_bookblock(product_img_array,"bookblockProduct");
     });
 
     $('#home').on('click', function () {
@@ -135,8 +141,12 @@ $(function () {
                 $("#slogan_game_easy").parent().css("z-index", "9999");
             }, 1000)
             $('#slogan_game_easy').fadeIn();
+
+
+
             $('#slogan').fadeIn();
             $('#trigger_button').trigger('click');
+            products_bookblock(easy_game_img_array,"bbEasy");
             //                $('#slogan_hard').hide();
             Page.init();
 
@@ -230,6 +240,7 @@ $(function () {
                     $('#slogan_game_easy').hide();
                     $("#slogan_game").parent().css("z-index", "999999");
                     $('.spinner').unbind('click');
+                    products_bookblock(product_img_array,'gameWin');
                     Page3.init()
                 }
             }
@@ -237,6 +248,7 @@ $(function () {
         else if ($(this).attr('id') == 'hard_game') {
             $('#slogan_game_hard').fadeIn();
             $('#slogan').fadeIn();
+            products_bookblock(hard_game_img_array,'bbHard')
             Page1.init1();
             $('#trigger_button').trigger('click');
 
@@ -330,6 +342,7 @@ $(function () {
                     $('#slogan_game_hard').hide();
                     $("#slogan_game").parent().css("z-index", "999999");
                     $('.spinner').unbind('click');
+                    products_bookblock(product_img_array,'gameWin');
                     Page3.init()
                 }
 
@@ -451,26 +464,49 @@ $(function () {
     }
 
 
-    function products_bookblock(product_img_array){
-        var seplen=product_img_array.length;   //images array
-        var carus = $('#bookblockProduct');        //will append images to this element
+    function products_bookblock(img_array,div_id){
+        var seplen=img_array.length;   //images array
+        var carus = $('#'+div_id);        //will append images to this element
         var imglist='';                    //we use this variable for optimization..
 
-        for(var i=0;i<seplen;i++){
-            if (i>0){
-                imglist+='<div class="bb-item"><a href="#"><img src="'+product_img_array[i]+'"/></a></div>';
-            }
-            else
-            {
-                imglist+='<div class="bb-item" style="display: block;"><a href="#"><img src="'+product_img_array[i]+'"/></a></div>';
-            }
+        if(div_id=="bookblockProduct" || div_id=="gameWin"){
+            for(var i=0;i<seplen;i++){
+                if (i>0){
+                    imglist+='<a href="#" class="close_parallax_btn" onclick="window.location.reload()" style="pointer-events:auto;"><div class="close_parallax_btn_text">X</div></a><div class="bb-item"><a href="#"><img src="'+img_array[i]+'"/></a></div>';
+                }
+                else
+                {
+                    imglist+='<a href="#" class="close_parallax_btn" onclick="window.location.reload()" style="pointer-events:auto;"><div class="close_parallax_btn_text">X</div></a><div class="bb-item" style="display: block;"><a href="#"><img src="'+img_array[i]+'"/></a></div>';
+                }
 
+            }
         }
+        else{
+            for(var i=0;i<seplen;i++){
+                if (i>0){
+                    imglist+='<a href="#" class="close_parallax_btn" data-toggle="modal" data-target="#confModal" style="pointer-events:auto;"><div class="close_parallax_btn_text">X</div></a><div class="bb-item"><a href="#"><img src="'+img_array[i]+'"/></a></div>';
+                }
+                else
+                {
+                    imglist+='<a href="#" class="close_parallax_btn" data-toggle="modal" data-target="#confModal" style="pointer-events:auto;"><div class="close_parallax_btn_text">X</div></a><div class="bb-item" style="display: block;"><a href="#"><img src="'+img_array[i]+'"/></a></div>';
+                }
+
+            }
+        }
+
 
         carus.html(imglist);
 
+//        $('.close_parallax_btn').on('click', function(){
+//            $("#try_it").trigger('click');
+//        })
+
         Page2.init()
     }
+
+
+
+
 
 
 
@@ -761,6 +797,22 @@ var Page3 = (function () {
 
 
 var state = 'home';
+
+function myFunction()
+{
+    var x;
+    var r=confirm("Are you sure you want to wimp out? It’s not that tough…");
+    if (r==true)
+    {
+//x="You pressed OK!";
+        window.location.reload()
+    }
+    else
+    {
+//x="You pressed Cancel!";
+    }
+//document.getElementById("demo").innerHTML=x;
+}
 
 function flipside(newState) {
     if (state == 'home') {
